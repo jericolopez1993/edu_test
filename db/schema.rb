26cabb38c0084e8a543540927b76754668d4f099
Jerico Lopez <jerico@maddington.net>
2018-07-04 05:37:51 +0800

    Third Push

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20180703185149) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
     t.string "resource_type"
-    t.integer "resource_id"
+    t.bigint "resource_id"
     t.string "author_type"
-    t.integer "author_id"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
@@ -50,8 +53,8 @@ ActiveRecord::Schema.define(version: 20180703185149) do
   end
 
   create_table "courses_professors", id: false, force: :cascade do |t|
-    t.integer "course_id", null: false
-    t.integer "professor_id", null: false
+    t.bigint "course_id", null: false
+    t.bigint "professor_id", null: false
     t.index ["course_id", "professor_id"], name: "index_courses_professors_on_course_id_and_professor_id"
   end
 
